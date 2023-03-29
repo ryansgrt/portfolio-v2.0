9fc328bde0bdc898843442dbfc02e6de2366a045
ADL.RIYANS <ADL.RIYANS@XL.CO.ID>
2023-03-29 15:47:31 +0700

    change photo

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,10 +135,10 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Riyan sugiarto and I enjoy creating things that live on the
+              internet. My interest in web development started back in 2014 when I decided to try
+              editing custom Tumblr themes — turns out hacking together a custom reblog button
+              taught me a lot about HTML &amp; CSS!
             </p>
 
             <p>
@@ -150,15 +150,6 @@ const About = () => {
               main focus these days is building accessible, inclusive products and digital
               experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
               clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
